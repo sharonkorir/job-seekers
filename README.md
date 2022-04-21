b45813aab77239b6fe726a35177c47ea7e359ce7
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-21 13:08:05 +0300

    fix folder structure

---
+++ README.md
@@ -0,0 +1,87 @@
+# jobseekers-hub
+
+This is a django application for rating CVs and elevator pitches. Users can upload their CV files to be rated based on professionalism, flow/structure and conciseness. Users can also upload their elevator pitches and get comments on them.  Users can also browse other CVs and elevator pitches and rate or comment respectively.
+
+## Screenshot
+
+<img src="" >
+
+### Dependencies
+
+In order for you to use the content on this repo ensure you have the following:
+
+- A computer that runs on either of the following; (Windows 7+, Linux, Mac OS)
+- Python 3.x+
+
+### Installing
+
+To get the code..
+
+1. Cloning the repository:
+
+```bash
+git clone https://github.com/sharonkorir/job-seekers.git
+```
+
+2. Navigate to the folder and install requirements. 
+
+```bash
+cd job-seekers
+python3 -m venv virtual
+pip install -r requirements.txt
+```
+
+3. Exporting Configurations
+
+```bash
+```
+
+4. Running the application
+
+```bash
+python3 manage.py runserver
+```
+
+Open the application on localhost `127.0.0.1:8000`.
+
+
+## Running the tests
+
+Testing the application
+
+```bash
+python3 manage.py test seekers
+```
+
+## Live link
+
+
+
+## Built With
+
+* [Python3.8](https://www.python.org/)
+* [Django](https://www.djangoproject.com/)
+* [Angular](https://angular.io/)
+
+## Known Bugs
+
+- No known bugs yet
+
+## Future features
+
+- Add feature that allows employers to search CVs based on categories, ordered by average rate.
+
+## Authors
+
+[Sharon Korir](https://github.com/sharonkorir)
+
+## Support and contact details
+
+If you run into any issues or have questions, ideas or concerns, reach out to me via e-mail, at csharonkorir@gmail.com
+
+## License
+
+https://choosealicense.com/licenses/mit/ 
+Copyright (c) {2022} **Sharon Korir**
+
+
---
+++ seekers/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+import random
+
+# Create your models here.
+class Profile(models.Model):
+    '''
+    Profile model acts as blueprint for all profile instances
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60, blank=True)
+    bio = models.TextField(max_length=200, blank=True)
+    profile_photo = CloudinaryField('image')
+    linkedIn_url = models.URLField(max_length = 150, null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.name)
+
+class Resume(models.Model):
+    '''
+    CV model acts as blueprint for all project instances
+    '''
+    cv = models.FileField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return str (self.profile.name)
+
+class Pitch(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    pitch = models.TextField(max_length=250)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    def __str__(self):
+        return self.pitch
+
+class Comment(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    pitch = models.ForeignKey(Pitch,on_delete=models.CASCADE)
+    comment = models.CharField(max_length=150)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    def __str__(self):
+        return self.comment
+
+RATE_CHOICES = [
+  (1, '1'),
+  (2, '2'),
+  (3, '3'),
+  (4, '4'),
+  (5, '5'),
+  (6, '6'),
+  (7, '7'),
+  (8, '8'),
+  (9, '9'),
+  (10, '10'),
+]
+
+class Rate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cv = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    conciseness = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+    professionalism = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+    flow = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return str(self.user)
