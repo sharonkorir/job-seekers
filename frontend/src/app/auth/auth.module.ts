ba91d115b73d53964e04c7140d98c15378cb1f5d
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-21 17:26:48 +0300

    create auth module

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
-  ]
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent
+  ],
 })
 export class AuthModule { }
