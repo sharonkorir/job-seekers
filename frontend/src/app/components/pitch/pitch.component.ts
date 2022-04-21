7c95641cf3eba583c3a9eada1b591d54e7a337fd
sharonkorir <sharon.korir@student.moringaschool.com>
2022-04-21 17:57:46 +0300

    create components and routes

---
+++ frontend/src/app/components/pitch/pitch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pitch',
+  templateUrl: './pitch.component.html',
+  styleUrls: ['./pitch.component.css']
+})
+export class PitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
